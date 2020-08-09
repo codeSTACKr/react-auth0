241df88ad1cd9e386fccac5498f325d6eb239398
dj <dman992266@gmail.com>
2020-08-09 02:41:49 -0400

    Integrate Auth0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import LoginButton from './components/LoginButton';
-import LogoutButton from './components/LogoutButton';
-import Profile from './components/Profile';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import "./App.css";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
   const { isLoading } = useAuth0();
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import "bootstrap/dist/css/bootstrap.min.css";
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
     !isAuthenticated && (
-      <button onClick={() => loginWithRedirect()}>
-        Log In
-      </button>
+      <div className="container">
+        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+          <h5 className="my-0 mr-md-auto font-weight-normal">ProductHub</h5>
+          <a
+            className="btn btn-outline-primary"
+            onClick={() => loginWithRedirect()}
+            href="#"
+          >
+            Login
+          </a>
+        </div>
+      </div>
     )
-  )
-}
+  );
+};
 
-export default LoginButton
+export default LoginButton;
