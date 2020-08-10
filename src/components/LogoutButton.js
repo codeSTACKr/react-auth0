b5db3f06a6619582827ef0a25d4d42e55e6eda48
dj <dman992266@gmail.com>
2020-08-10 06:54:28 -0400

    Deploy website to https://erick-pacheco.github.io/react-auth0/#

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const LogoutButton = () => {
   const { logout, isAuthenticated } = useAuth0();
 
   return (
     isAuthenticated && (
-      <button onClick={() => logout()}>
+      <button
+        className="btn btn-outline-primary logout"
+        onClick={() => logout()}
+      >
         Log Out
       </button>
     )
-  )
-}
+  );
+};
 
-export default LogoutButton
+export default LogoutButton;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import JSONPretty from 'react-json-pretty';
+import JSONPretty from "react-json-pretty";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
 
   return (
-    isAuthenticated && ( 
-     <div>
-        <img src={user.picture} alt={user.name} />
+    isAuthenticated && (
+      <div>
+        <img
+          className="img-profile rounded-circle"
+          src={user.picture}
+          alt={user.name}
+        />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
         <JSONPretty data={user} />
         {/* {JSON.stringify(user, null, 2)} */}
       </div>
     )
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
