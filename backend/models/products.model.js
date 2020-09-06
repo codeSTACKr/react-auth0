1f542484dc1e7bc68a17d4d67143bfc733f35b26
Katherine Shimkus <linarespachecoerick@gmail.com>
2020-09-06 19:20:46 -0400

    Completes product Schema and update/POST test - successful

--- backend/models/products.model.js
+++ backend/models/products.model.js
@@ -1,34 +1,76 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const productsSchema = new Schema({
-  sku: String,
-  quantity: Number,
-  name: String,
-  nameTranslated: {
-    en: String,
-    es: String
-  },
-  price: Number,
-  compareToPrice: Number,
-  isShippingRequired: Boolean,
-  categoryIds: [Number],
-  weight: Number,
-  enabled: Boolean,
-  description: String,
-  productClassId: Number,
-  created: String,
-  fixedShippingRateOnly: Boolean,
-  fixedShippingRate: Number,
-  brand: String,
-  tax: {
-    enabledManualTaxes: [
-      Number
-    ]
-  },
-  optionsType: String,
-     
-});
+const productsSchema = new Schema(
+  {
+    sku: String,
+    quantity:Number,
+    name: String,
+    nameTranslated: {
+      en: String,
+      es: String
+    },
+    price:Number,
+    compareToPrice:Number,
+    isShippingRequired:Boolean,
+    categoryIds: [
+      Array
+    ],
+    weight:Number,
+    enabled:Boolean,
+    description: String,
+    productClassId: Number,
+    created:Date,
+    fixedShippingRateOnly:Boolean,
+    fixedShippingRate:Number,
+    tax: {
+      enabledManualTaxes: [
+        String
+      ]
+    },  
+    options: [
+      {
+        type: String,
+        name:String,
+        nameTranslated: {
+          en: String,
+          es: String
+        },
+        choices: [
+          {
+            text:String,
+            textTranslated: {
+              en: String,
+              es:String
+            },
+            priceModifier:Number,
+            priceModifierType: Number
+          },
+          {
+            text: String,
+            textTranslated: {
+              en: String,
+              es:String
+            },
+            priceModifier: Number,
+            priceModifierType:String
+          }
+        ],
+          defaultChoice:Number,
+          required: Boolean
+      }
+    ],
+    shipping: {
+      type: String,
+      methodMarkup:Number,
+      flatRate: Number,
+      disabledMethods: [
+        Array
+      ],
+      nabledMethods: [Array]
+    }
+  }
+);
 
 const Products = mongoose.model("Products", productsSchema);
 
